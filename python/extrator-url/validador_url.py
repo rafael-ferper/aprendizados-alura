f90b38e9ec0bf8e65ab3555fc3a51fae9f9c1fbf
Rafael Fernandes Pereira <rtio.fernandesp@gmail.com>
2023-10-11 03:58:48 -0300

    enh: adicao do projeto extrator_url

---
+++ python/extrator-url/validador_url.py
@@ -0,0 +1,25 @@
+# Exemplos de URLs válidas:
+#     bytebank.com/cambio
+#     bytebank.com.br/cambio
+#     www.bytebank.com/cambio
+#     www.bytebank.com.br/cambio
+#     http://www.bytebank.com/cambio
+#     http://www.bytebank.com.br/cambio
+#     https://www.bytebank.com/cambio
+#     https://www.bytebank.com.br/cambio
+#
+# Exemplos de URL inválidas:
+#     https://bytebank/cambio
+#     http://bytebank.naoexiste/cambio
+#     ht:bytebank.naoexiste/cambio
+
+import re
+
+url = "www.bytebank.com.br/cambio"
+padrao_url = re.compile('(hrrp(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("A URL não é válida.")
+
+print("A URL é válida.")
